0c468074ffe2d9defcc6dea2e8aa4f01774fdac5
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-10 20:21:28 +0000

    beryllium-user-10-QKQ1.190828.002-V11.0.4.0.QEJMIXM-release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -12,12 +12,12 @@ PRODUCT_BRAND := xiaomi
 PRODUCT_DEVICE := beryllium
 PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_NAME := lineage_beryllium
-PRODUCT_MODEL := POCO F1
+PRODUCT_MODEL := POCOPHONE F1
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := beryllium
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="beryllium-user 9 PKQ1.180729.001 V11.0.5.0.PEJMIXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.4.0.QEJMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V11.0.5.0.PEJMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.4.0.QEJMIXM:user/release-keys
